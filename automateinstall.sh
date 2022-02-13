8bb450f75b0c322a3bbb87d038ecb15ac23869b7
20tdunn <73849793+20tdunn@users.noreply.github.com>
2022-02-12 21:16:00 -0600

    Update automateinstall.sh

--- automateinstall.sh
+++ automateinstall.sh
@@ -2,10 +2,10 @@
 
 mkdir Desktop Documents Downloads Pictures Videos .config/ .hidden/
 
-mv -r swayconfigs /home/trevor/.hidden/
+mv swayconfigs /home/trevor/.hidden/
 
-mv -r /home/trevor/.hidden/swayconfigs/Wallpapers /home/trevor/Pictures/
+mv /home/trevor/.hidden/swayconfigs/Wallpapers /home/trevor/Pictures/
 
-mv -r /home/trevor/.hidden/swayconfigs/alacritty /home/trevor/.hidden/swayconfigs/wofi /home/trevor/.hidden/swayconfigs/waybar /home/trevor/.hidden/swayconfigs/sway /home/trevor/.hidden/swayconfigs/fish  /home/trevor/.config/
+mv /home/trevor/.hidden/swayconfigs/alacritty /home/trevor/.hidden/swayconfigs/wofi /home/trevor/.hidden/swayconfigs/waybar /home/trevor/.hidden/swayconfigs/sway /home/trevor/.hidden/swayconfigs/fish  /home/trevor/.config/
 
-pacman -S sway alacritty firefox wofi waybar fish git nemo zip unzp kdeconnect tlp pulseaudio pavucontrol obs-studio flameshot vlc thunderbird discord signal-desktop vim htop
+pacman -S sway alacritty firefox wofi waybar fish git nemo zip unzip kdeconnect tlp pulseaudio pavucontrol obs-studio flameshot vlc thunderbird discord signal-desktop vim htop
